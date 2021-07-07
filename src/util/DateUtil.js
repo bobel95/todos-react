52bed4913bfe45f2f5571589f79fd3703c600c76
Iulian Popa <bobel2007@gmail.com>
2021-07-07 17:13:43 +0300

    Add date and time to string util method

--- src/util/DateUtil.js
+++ src/util/DateUtil.js
@@ -11,4 +11,15 @@ const getTimeLeft = date => {
     return {daysLeft: days, hoursLeft: hours};
 }
 
-export {getTimeLeft};
+const getDateAndTimeString = date => {
+    const year = date.getFullYear();
+    const month = date.getMonth() + 1;
+    const day = date.getDate();
+
+    const hours = date.getHours();
+    const minutes = date.getMinutes();
+
+    return `${day}.${month}.${year} - ${hours}:${minutes}`;
+}
+
+export {getTimeLeft, getDateAndTimeString};
