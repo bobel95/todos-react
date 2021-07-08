e72540c4e97d00a103e76395566ad46915db78a4
Iulian Popa <bobel2007@gmail.com>
2021-07-08 17:29:45 +0300

    Add sorting by due date

--- src/components/TaskList.js
+++ src/components/TaskList.js
@@ -2,6 +2,9 @@ import React, {useState, useEffect} from 'react';
 import {getTasks} from "../api/tasks";
 import Task from "./Task";
 import {Button, Paper, styled} from "@material-ui/core";
+import ArrowUpwardIcon from '@material-ui/icons/ArrowUpward';
+import ArrowDownwardIcon from '@material-ui/icons/ArrowDownward';
+
 
 const TaskList = () => {
 
@@ -16,12 +19,39 @@ const TaskList = () => {
         justifyContent: "center",
     });
 
+    const MyButton = styled(Button) ({
+        textTransform: "none",
+    })
+
     const [tasks, setTasks] = useState([]);
+    const [sorting, setSorting] = useState(null);
     const [state, setState] = useState(false);
 
     const reloadTasks = () => {
         setState(!state);
     }
+
+    const handleSorting = () => {
+        setSorting(sorting === "DESC" ? "ASC" : "DESC");
+    }
+
+    const getSortingIcon = () => {
+        return sorting === "DESC"
+            ? <ArrowUpwardIcon/>
+            : <ArrowDownwardIcon/>
+    }
+
+    useEffect(() => {
+        // if (sorting !== null) {
+            const comparator = sorting === "ASC"
+                ? (a, b) => (new Date(a.dueDate) < new Date(b.dueDate)) ? 1 : -1
+                : (a, b) => (new Date(a.dueDate) > new Date(b.dueDate)) ? 1 : -1;
+
+            const sortedTasks = [...tasks].sort(comparator);
+            setTasks(sortedTasks);
+        // }
+    }, [sorting]);
+
 
     useEffect(() => {
         getTasks()
@@ -31,7 +61,9 @@ const TaskList = () => {
     return (
         <TaskListContainer>
             <div style={{borderBottom: "2px solid #aaa", width: "80%", margin: "1rem"}}>
-                <Button>sort</Button>
+                <MyButton onClick={() => {handleSorting()}}>
+                    Sort by Due Date {getSortingIcon()}
+                </MyButton>
             </div>
             {tasks.map(task => (
                 <Task data={task} reloadTasks={reloadTasks} />
